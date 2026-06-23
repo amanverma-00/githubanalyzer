65a677aa0e9f0191ec28e210f0e2376fa53a2e0c
amanverma-00 <vermaaman1072@gmail.com>
2026-06-23 21:05:25 +0530

    feat: add cors middleware to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import connectDb from './config/db.js';
 import productRoutes from './routes/product.routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
